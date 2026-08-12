327fa08146110f543b6bbc71dde3ebec98341484
Victor Marques <victor.pecine.marquesp@gmail.com>
2026-08-12 18:41:00 -0300

    Model Deposit criado

---
+++ app/Models/Deposit.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Deposit extends Model {
+    
+    protected $fillable = [
+        'safe_id',
+        'coin_id',
+        'quantity',
+        'value_cents'
+    ];
+
+    public function safe() : BelongsTo {
+        return $this->belongsTo(Safe::class);
+    }
+
+}
--- app/Models/Safe.php
+++ app/Models/Safe.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Safe extends Model
 {
@@ -25,5 +26,9 @@ class Safe extends Model
 
     public function currency() : BelongsTo {
         return $this->belongsTo(Currency::class);
+    }
+
+    public function deposits() : HasMany {
+        return $this->hasMany(Deposit::class);
     }
 }
